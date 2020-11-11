46dbd0be3a7e383dc1aa0481ef5e8cc623ecc29e
soucieux9697 <soucieux9697@gmail.com>
2020-11-10 21:55:39 -0500

    Added Welcome page

---
+++ app/src/main/java/com/example/alphadrawer/WelcomePageActivity.java
@@ -0,0 +1,24 @@
+package com.example.alphadrawer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.alphadrawer.ui.login.LoginFragment;
+
+public class WelcomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+    }
+
+    public void logInAction(View view){
+        startActivity(new Intent(WelcomePageActivity.this, LoginActivity.class));
+    }
+}
+
