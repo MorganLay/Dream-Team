0240f46b5b7bcc744bc94f3eaba7159d3b6a608c
soucieux9697 <soucieux9697@gmail.com>
2020-12-07 01:32:10 -0500

    use database to store user info

--- app/src/main/java/com/example/alphadrawer/NewAccountActivity.java
+++ app/src/main/java/com/example/alphadrawer/NewAccountActivity.java
@@ -2,22 +2,29 @@ package com.example.alphadrawer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.alphadrawer.ui.newAccount.newAccountFragment;
+import com.example.alphadrawer.ui.user.user;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NewAccountActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private static final String TAG = "NewAccountActivity";
+    private DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
         setContentView(R.layout.content_main);
         newAccountFragment newAccountFragment = new newAccountFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, newAccountFragment).commit();
@@ -27,16 +34,14 @@ public class NewAccountActivity extends AppCompatActivity {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if(task.isSuccessful()) {
-                        Toast.makeText(NewAccountActivity.this, "Account created successfully",
-                                Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(NewAccountActivity.this, MainActivity.class);
-                        intent.putExtra("user", true);
-                        intent.putExtra("userName", userName);
-                        intent.putExtra("email", email);
-                        startActivity(intent);
+                        Log.d(TAG,"Account created successfully");
+                        user newUser = new user(userName, email);
+                        String userId = mAuth.getCurrentUser().getUid();
+                        database.child("users").child(userId).setValue(newUser);
+                        startActivity(new Intent(NewAccountActivity.this, MainActivity.class));
                     } else {
                         Toast.makeText(NewAccountActivity.this, "Email address exists, you can login directly",
-                                Toast.LENGTH_LONG).show();
+                                Toast.LENGTH_SHORT).show();
                     }
                 });
     }
