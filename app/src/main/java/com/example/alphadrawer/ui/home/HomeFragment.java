111401280f4edff2aea07aea9d02aea2ad833697
Morgan Lay <morgan.lay11@gmail.com>
2020-12-06 20:22:04 -0500

    Weather widget 2.0

--- app/src/main/java/com/example/alphadrawer/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/alphadrawer/ui/home/HomeFragment.java
@@ -35,8 +35,8 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = root.findViewById(R.id.text_home);
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         findWeather();
 
