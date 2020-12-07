cb2f3458307b06b7c152c61a0231a8a28f7e6481
soucieux9697 <soucieux9697@gmail.com>
2020-12-07 17:36:25 -0500

    clean up profile page

---
+++ app/src/main/java/com/example/alphadrawer/ui/profile/profileFragment.java
@@ -0,0 +1,24 @@
+package com.example.alphadrawer.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.alphadrawer.R;
+
+public class profileFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_slideshow, container, false);
+    }
+}
